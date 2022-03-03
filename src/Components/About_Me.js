baf53a0969e66c703f0f89d59fca08069f97145f
saeediqbalofficial <SaeedIqbalofficial@gmail.com>
2022-03-03 22:37:44 +0500

    Update About_Me.js

--- src/Components/About_Me.js
+++ src/Components/About_Me.js
@@ -18,7 +18,7 @@ const About_Me = () => {
           page editors now use Lorem Ipsum as their default model text, and a
           search for 'lorem ipsum' will uncover many web sites still in their
           infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like).
+          accident, sometimes on purpose (injected humour and the like). Yeah
         </p>
       </div>
     </div>
